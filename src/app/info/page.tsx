0cb597e066347dbc7a8099dcb8b1b7a5b3d1df41
rkdwd <159889372+rkdwd@users.noreply.github.com>
2024-10-02 19:53:03 +0900

    Add files via upload

--- src/app/info/page.tsx
+++ src/app/info/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
-        src="/info/assets/tri_gimbap.jpg"
+        src="/tri_gimbap.jpg"
         width={300}
         height={300}
         alt="What the author likes the most"
---
+++ src/app/introduce/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hello from '../components/Hello';
+
+function page() {
+  return (
+    <div>       
+      <Hello color="red"/>
+    </div>
+  );
+}
+
+export default page;
